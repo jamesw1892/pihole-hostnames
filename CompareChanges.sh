a7245c9d99632d5032788e182c1ab1d0a730ba5a
jamesw1892 <gingersemaj1892@gmail.com>
2023-06-13 17:59:51 +0100

    Make compare script work with any script

--- CompareChanges.sh
+++ CompareChanges.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Export the tables, rename them, update hostnames, and then export the tables
-# again so they can be compared. This can be helpful for catching mistakes as
-# the old state of the tables is known so it can be reset to this. Note we don't
-# do this to dhcp.leases since this doesn't change when updating hostnames.
+# Export the tables, rename them, run the given script, and then export
+# the tables again so they can be compared. This can be helpful for catching
+# mistakes as the old state of the tables is known so it can be reset to this.
+# Note we don't do this to dhcp.leases since this doesn't change.
+
+if [ $# -lt 1 ]; then
+    echo "Must enter the script to run to compare before and after such as './UpdateHostnames.sh'"
+fi
 
 EXPORT_DIR="exported"
 
@@ -10,5 +14,5 @@ EXPORT_DIR="exported"
 for table in network_addresses network aliasclient client_by_id ; do
     mv "$EXPORT_DIR/$table.csv" "$EXPORT_DIR/${table}_old.csv"
 done
-./UpdateHostnames.sh
+$1
 ./ExportNetworkInfo.sh
